055ecfe4b3e588a38310b7e291ea13c6a1cc49f3
usehooke <nandof83@gmail.com>
2026-01-28 14:07:01 -0300

    fix(next.config): remove remotePatterns para permitir imagens locais

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com', // Mantém as fotos dos produtos
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co', // Adiciona permissão para o diagrama de medidas
-      },
-    ],
-  },
+ 
 };
 
 export default nextConfig;
