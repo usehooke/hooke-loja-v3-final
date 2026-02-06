99d6b01efca3467f49187fa9ea57376008b04938
usehooke <nandof83@gmail.com>
2026-02-06 10:47:24 -0300

    Fix: Corrigindo loop infinito no Carrinho e package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      { protocol: 'https', hostname: '**' },
+    ],
+    unoptimized: false,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
