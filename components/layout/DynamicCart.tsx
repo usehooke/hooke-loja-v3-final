42eb66e62a6a844f5983b1a948403a3baad8039c
usehooke <nandof83@gmail.com>
2026-02-23 14:29:56 -0300

    fix(cart): load CartSheet dynamically instead of CartSidebar

--- components/layout/DynamicCart.tsx
+++ components/layout/DynamicCart.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from 'next/dynamic';
 
-const DynamicCart = dynamic(() => import('@/components/shop/CartSidebar'), {
+const DynamicCart = dynamic(() => import('@/components/cart/CartSheet'), {
   ssr: false,
   loading: () => null, // You can add a loading spinner here if you want
 });
